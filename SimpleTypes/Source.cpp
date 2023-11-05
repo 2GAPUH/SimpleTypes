a29683c3b5f769eedf8af5f49d8063cd2e0dbbf9
2GAPUH <svetoslavvolkov2004@gmail.com>
2023-11-06 00:12:55 +0300

    0

--- SimpleTypes/Source.cpp
+++ SimpleTypes/Source.cpp
@@ -5,12 +5,60 @@
 #include "Complex.h"
 #define BASE_SIZE 100
 
-void Sort()
+void Append(IManagable** array, int& type2, int& size)
 {
+	int main = 0, sub = 0;
+	char buf[BUFFER_SIZE] = { NULL };
+	Point2 startPoint1 = { 0, 0 };
+	Point2 endPoint1 = { 0, 0 };
+	Point3 startPoint = { 0, 0, 0 };
+	Point3 endPoint = { 0, 0, 0 };
 
+	switch (type2)
+	{
+	case 1:
+		std::cout << "Enter main: ";
+		std::cin >> main;
+		std::cout << "Enter sub: ";
+		std::cin >> sub;
+		array[size++] = new Complex(main, sub);
+		break;
+	case 2:
+		std::cout << "Enter message: ";
+		while (getchar() != '\n');
+		std::cin.getline(buf, BUFFER_SIZE);
+		array[size++] = new Text(buf);
+		break;
+	case 3:
+		std::cout << "Enter start x: ";
+		std::cin >> startPoint1.x;
+		std::cout << "Enter start y: ";
+		std::cin >> startPoint1.y;
+		std::cout << "Enter end x: ";
+		std::cin >> endPoint1.x;
+		std::cout << "Enter end y: ";
+		std::cin >> endPoint1.y;
+		array[size++] = new Vector2d(startPoint1, endPoint1);
+		break;
+	case 4:
+		std::cout << "Enter start x: ";
+		std::cin >> startPoint.x;
+		std::cout << "Enter start y: ";
+		std::cin >> startPoint.y;
+		std::cout << "Enter start z: ";
+		std::cin >> startPoint.z;
+		std::cout << "Enter end x: ";
+		std::cin >> endPoint.x;
+		std::cout << "Enter end y: ";
+		std::cin >> endPoint.y;
+		std::cout << "Enter end z: ";
+		std::cin >> endPoint.z;
+		array[size++] = new Vector3d(startPoint, endPoint);
+		break;
+	}
 }
 
-void Min()
+void Min(IManagable** array,  int& size)
 {
 
 }
@@ -61,7 +109,7 @@ void Join()
 
 }
 
-void Append()
+void Sort()
 {
 
 }
@@ -78,13 +126,13 @@ int main()
 	IManagable* vector2d[BASE_SIZE] = { nullptr };
 	IManagable* vector3d[BASE_SIZE] = { nullptr };
 	int textSize = 0;
-	int cimplexSize = 0;
+	int complexSize = 0;
 	int vector2dSize = 0;
 	int vector3dSize = 0;
 	int type2 = 0;
 	int type1 = 1;
 	std::string instruction1 = "1 - Sort\n2 - Minimum value\n3 - Maximum value\n4 - Find value\n5 - Print line\n\
-6 - Print column\n 7 - Total sum\n8 - Number in range\n9 - Print peaks\n10 - Make a copy\n11 - Add\n\
+6 - Print column\n 7 - Total sum\n8 - Number in range\n9 - Print peaks\n10 - Make a copy\n11 - Append\n\
 12 - Other add\n13 - Print unique\n0 - Exit\nEnter your choice: ";
 	std::string instruction2 = "1 - Complex array \n2 - Text array \n3 - Vector2d array \n4 - Vector3d array \nEnter your choice: ";
 
@@ -242,12 +290,16 @@ int main()
 			switch (type2)
 			{
 			case 1:
+				Append(complex, type2, complexSize);
 				break;
 			case 2:
+				Append(text, type2, textSize);
 				break;
 			case 3:
+				Append(vector2d, type2, vector2dSize);
 				break;
 			case 4:
+				Append(vector3d, type2, vector3dSize);
 				break;
 			}
 			break;
